3a34eac5c9568f8f1d40d60c7a71ecdfe52b3727
Alf0nce <alfoncekngetich@gmail.com>
2025-07-16 12:42:58 +0300

    Final Submission

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import react from 'react';
-import ReadDOM from 'react-dom/client';
-import { ThemeProvider } from './contexts/ThemeContext';
+// main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <><React.StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
-  </React.StrictMode><StrictMode>
-      <App />
-    </StrictMode></>,
-);
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
